3564710e31f0c6ddc44b3c617772b95d9b47846c
AfonsoCastro1983 <afonso.castro@gmail.com>
2024-12-01 15:54:42 -0400

    Forçar credenciais

--- src/infra/sqs/sqs.ts
+++ src/infra/sqs/sqs.ts
@@ -2,12 +2,30 @@ import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { IEnvioFilaMensageria } from "../../application/interfaces/IEnvioFilaMensageria";
 import { StatusPedido } from "../../shared/enums/StatusPedido";
 import { v4 as uuidv4 } from 'uuid';
+import dotenv from "dotenv";
 
 export class FilaSQS implements IEnvioFilaMensageria{
     private _sqsClient: SQSClient;
     
     constructor () {
-        this._sqsClient = new SQSClient({ region: "us-east-2" });
+        dotenv.config();
+
+        let aws_ski: string = "";
+        let aws_sak: string = "";
+        if (process.env.AWS_ACCESS_KEY_ID) {
+            aws_ski = process.env.AWS_ACCESS_KEY_ID;
+        }
+        if (process.env.AWS_SECRET_ACCESS_KEY) {
+            aws_sak = process.env.AWS_SECRET_ACCESS_KEY;
+        }
+        this._sqsClient = new SQSClient({
+            region: "us-east-2",
+            credentials: {
+                accessKeyId: aws_ski,
+                secretAccessKey: aws_sak
+            }
+        });
+        
     }
 
     public async envioFila(pedido: string, status: StatusPedido): Promise<boolean> {
